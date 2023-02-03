0a6f3c5d304d6bef76f2afc9da0cb5cb84926fd7
Nata Braun <pushistik2030@gmail.com>
2023-02-03 16:59:29 +0300

    Task is solved

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
+
+
     public void printGreatest(int first, int second) {
+        System.out.println(first > second ? first : second);
     }
 }
